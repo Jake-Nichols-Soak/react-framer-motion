2c9de8331bad866d751072d4dab3e62d74ef7ebe
Jake-Nichols-Soak <88318603+Jake-Nichols-Soak@users.noreply.github.com>
2023-02-15 13:14:28 +0000

    Initial commit

---
+++ src/app/hooks/paths.js
@@ -0,0 +1,13 @@
+import {useConfig} from '@/app/contexts/ConfigurationContext';
+
+export const useImagePath = () => {
+  const { getConfigValue } = useConfig();
+
+  return (src) => `${getConfigValue('imagePath')}/${src.replace(/^\/+/, '')}`;
+}
+
+export const useDataPath = () => {
+  const { getConfigValue } = useConfig();
+
+  return (src) => `${getConfigValue('dataPath')}/${src.replace(/^\/+/, '')}`;
+}
